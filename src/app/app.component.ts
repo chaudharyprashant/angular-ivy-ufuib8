8177b87f5d723c07eb3d4be1fbc7e85fc9253184
chaudharyprashant <pk15196@gmail.com>
2021-08-08 12:58:05 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component, VERSION } from '@angular/core';
+import * as data from './data.json';
+
+interface Candidate {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  candidates: Candidate[] = (data as any).default;
+  //candidates: Candidate[] = data;
+  ngOnInit() {
+    console.log(this.candidates);
+  }
 }
